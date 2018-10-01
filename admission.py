21905e81c44e9afac7448f0d8ec4cfc1fac8d08d
niftyakash <43758066+niftyakash@users.noreply.github.com>
2018-10-01 22:43:39 +0530

    Revert "Add files via upload"

--- admission.py
+++
@@ -1,9 +0,0 @@
-def admission(p,c,m,avg):
-        
-        if (p>=80 and c>=80 and m>=80 ):
-                x=1
-                print(x)
-        else:
-                x=-1
-                print(x)
-                return x
